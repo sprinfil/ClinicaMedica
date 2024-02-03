58b5bd970d09b6388c0d0a1d93e0d71de625d546
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2024-02-02 17:20:33 -0700

    detalle imagenes

--- app/Http/Controllers/subirImagenController.php
+++ app/Http/Controllers/subirImagenController.php
@@ -7,6 +7,7 @@ use App\Models\Paciente;
 use App\Models\Tratamiento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class subirImagenController extends Controller
 {
@@ -17,10 +18,15 @@ class subirImagenController extends Controller
     }
 
     public function store_clinica(Request $request){
-        $tratamiento = Tratamiento::find($request->tratamiento_id);
+       $tratamiento = Tratamiento::find($request->tratamiento_id);
 
         //guardar la imagen y obtener ruta
-        $imagen = $request->file('file')->store('public/clinica/'.$tratamiento->id);
+        //eliminar
+        $rutaBase = storage_path('app/public/clinica/'.$tratamiento->id);
+        File::makeDirectory($rutaBase, 0775, true, true);
+        //eliminar
+
+        $imagen = $request->file('file')->store('public/clinica/'.$tratamiento->id.'/');
 
         //convertir la ruta en vez de public a storage
         $url_imagen = Storage::url($imagen);
@@ -36,8 +42,13 @@ class subirImagenController extends Controller
     public function store_radiografia(Request $request){
         $tratamiento = Tratamiento::find($request->tratamiento_id);
 
+        //eliminar
+        $rutaBase = storage_path('app/public/radiografia/'.$tratamiento->id);
+        File::makeDirectory($rutaBase, 0775, true, true);
+        //eliminar
+
         //guardar la imagen
-        $imagen = $request->file('file')->store('public/radiografia/'.$tratamiento->id);
+        $imagen = $request->file('file')->store('public/radiografia/'.$tratamiento->id.'/');
 
         //convertir la ruta en vez de public a storage
         $url_imagen = Storage::url($imagen);
